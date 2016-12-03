524c0e3a08bfeb0edc5866505d33b008ecd3971c
YohannBethoule <yoyoteva@gmail.com>
2016-12-03 20:09:40 +0100

    Template bootstrap vide

---
+++ main.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <link href="C:\Users\yoyot\Desktop\bootstrap-3.3.7-dist\css" rel="stylesheet"/>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoyot
+ * Date: 17/11/2016
+ * Time: 14:23
+ */
+
+?>
+</body>
+</html>
